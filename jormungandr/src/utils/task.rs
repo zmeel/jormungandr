89b11806adce8608196283eb62f055a0b1ccd950
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2020-01-27 17:36:23 +0200

    Log spawned futures
    Logging of futures is at trace level.

--- jormungandr/src/utils/task.rs
+++ jormungandr/src/utils/task.rs
@@ -169,11 +169,19 @@ impl TokioServiceInfo {
     }
 
     /// spawn a future within the service's tokio executor
-    pub fn spawn<F>(&self, future: F)
+    pub fn spawn<F>(&self, name: &'static str, future: F)
     where
         F: Future<Item = (), Error = ()> + Send + 'static,
     {
-        tokio::spawn(future);
+        let logger = self.logger.clone();
+        trace!(logger, "spawning {}", name);
+        tokio::spawn(future.then(move |res| {
+            match res {
+                Ok(()) => trace!(logger, "{} finished successfully", name),
+                Err(()) => trace!(logger, "{} finished with error", name),
+            }
+            res
+        }));
     }
 }
 
